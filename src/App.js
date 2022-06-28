00cea14b6a7bbd421eb890982f7f5aa48f5e6743
thefidanabdulla <thefidanabdulla@gmail.com>
2022-06-28 14:15:45 +0400

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Comments from './components/Comments';
+import Posts from './components/Posts';
+import Users from './components/Users';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app__app">
+      <Routes>
+        <Route path='/users' element={<Users />}/>
+        <Route path='/posts' element={<Posts />}/>
+        <Route path='/comments' element={<Comments />}/>
+      </Routes>
     </div>
   );
 }
